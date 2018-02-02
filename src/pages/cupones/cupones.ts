22f3b7ef2563ae88952a7a842ea85e7116c22f0c
Eduardo Gomez <kalienrique40@gmail.com>
2018-02-02 09:48:37 -0600

    cambios en vista de cupones

--- src/pages/cupones/cupones.ts
+++ src/pages/cupones/cupones.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PopoverController } from 'ionic-angular';
+import { MyApp } from '../../app/app.component';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {LoadingController } from 'ionic-angular/index';
 
 @IonicPage()
 @Component({
@@ -8,15 +14,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CuponesPage {
   cupones: Array<any>;
+  items:Array<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.cupones = [
+  constructor(public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController,public http: Http,public loadingCtrl: LoadingController) {
+    this.items = [
       { bg: "url(assets/images/cupones/01.jpeg)", txt: "Restaurante Piscon", desc: "20%" },
       { bg: "url(assets/images/cupones/02.jpg)", txt: "Restaurant Comida Peruana - Limón", desc: "10%" },
       { bg: "url(assets/images/cupones/03.jpg)", txt: "Bebidas Pisco", desc: "10%" },
       { bg: "url(assets/images/cupones/04.jpg)", txt: "Cupón 1", desc: "25%" },
       { bg: "url(assets/images/cupones/05.jpg)", txt: "Cupón 2", desc: "20%" }
     ];
+
+    this.http.get('http://dcsocialmarketing.com/previo/PiscosAdmin/index.php/api/cupones_descuento?userId=2').map(res => res.json()).subscribe(data => {
+      this.items = data;
+      let loading = this.loadingCtrl.create({
+        spinner: 'hide',
+        content: `
+          <div class="custom-spinner-container">
+            <div class="custom-spinner-box"></div>
+          </div>`,
+        duration: 5000
+      });
+
+      loading.onDidDismiss(() => {
+        console.log('Dismissed loading');
+      });
+
+      loading.present();
+
+      console.log(this.items);
+  });
   }
 
   ionViewDidLoad() {
--- src/pages/eventos/eventos.ts
+++ src/pages/eventos/eventos.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PopoverController } from 'ionic-angular';
+import { MyApp } from '../../app/app.component';
+
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {LoadingController } from 'ionic-angular/index';
 
 @IonicPage()
 @Component({
@@ -8,13 +14,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EventosPage {
   eventos: Array<any>;
+  items:Array<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public popoverCtrl: PopoverController,public http: Http,public loadingCtrl: LoadingController) {
     this.eventos = [
       { bg: "url(assets/images/cupones/01.jpeg)", txt: "Sigue la fiesta donde sea...", fecha: "Viernes 29 | Dic | 2017", precio: "S/ 64,00.00", dir: "Calle Hércules 121, San Roque, Surco Lima, Perú" },
       { bg: "url(assets/images/cupones/02.jpg)", txt: "Festival del Pisco", fecha: "Domingo 14 | Ene | 2018", precio: "S/ 50,00.00", dir: "Calle Hércules 121, San Roque, Surco Lima, Perú" },
       { bg: "url(assets/images/cupones/03.jpg)", txt: "Noche de Dj's", fecha: "Viernes 19 | Ene | 2018", precio: "S/ 20,00.00", dir: "Calle Hércules 121, San Roque, Surco Lima, Perú" }
     ];
+
+    MyApp.callfrom='home'
+
+        this.http.get('http://dcsocialmarketing.com/previo/PiscosAdmin/index.php/api/noticias').map(res => res.json()).subscribe(data => {
+          this.items = data;
+          let loading = this.loadingCtrl.create({
+            spinner: 'hide',
+            content: `
+              <div class="custom-spinner-container">
+                <div class="custom-spinner-box"></div>
+              </div>`,
+            duration: 5000
+          });
+
+          loading.onDidDismiss(() => {
+            console.log('Dismissed loading');
+          });
+
+          loading.present();
+
+          console.log(this.items);
+      });
   }
 
   ionViewDidLoad() {
